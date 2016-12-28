51c2b967d83156724cb0aa1f75cc1852fb446734
Denis Lukyanov <kakawka05@gmail.com>
2016-12-28 16:21:26 +0300

    initial commit

---
+++ app/models/spree/order_decorator.rb
@@ -0,0 +1,16 @@
+require 'beaneater'
+Spree::Order.class_eval do
+  def decrease_quantity_in_core
+    beanstalk = Beaneater.new('localhost:11300')
+    tube = beanstalk.tubes["tube"]
+
+    Spree::Order.last.line_items.each do |li|
+      tube.put({product_id: li.product.id, quantity: li.quantity}.to_json)
+    end
+
+    beanstalk.close
+  end
+end
+
+Spree::Order.state_machine.after_transition to: :complete,
+                                            do: :decrease_quantity_in_core
