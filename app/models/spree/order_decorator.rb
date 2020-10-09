8e0178cb69d06c354bd4955d7b195d10f9e7f682
Denis Gavrilin <den.gavrilin1@gmail.com>
2020-10-10 00:36:06 +0300

    Added order number to initializer

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -58,6 +58,7 @@ class MQOrderSerializer
     def serialize(order)
       {
         id: order.id,
+        number: order.number.presence.to_s,
         total: order.total.to_f,
         shipping_total: order.shipment_total.to_f,
         line_items: order.line_items.map { |l| line_item(l) }
